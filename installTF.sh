3229e4d4abc6a7307727d409d90f03923d10a555
Justin West <mxracer409@hotmail.com>
2023-10-17 13:25:55 -0700

    Update installTF.sh

--- installTF.sh
+++ installTF.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 #simple bash startup script to install openssh, allow ssh through FW and install terraform. 
 
-sudo su - 
+apt-get --assume-yes update
+apt-get --assume-yes upgrade -y
 
-apt-get update
-
-apt-get upgrade -y
-
-apt-get install openssh-server -y
+apt-get --assume-yes install openssh-server -y
 
 ufw allow ssh
+ufw enable 
 
-ufw enable
-
-#sudo su -
-
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+apt-get --assume-yes install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+reset
 
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update
+apt-get update
 
-sudo apt-get install terraform
+apt-get --assume-yes install terraform
 
-sudo terraform --version 
+terraform --version 
